2e2dfd1a68038139ad7cf064882cb938ccc92c86
Jon Neale <jon.neale@uswitch.com>
2012-06-06 13:57:23 +0100

    Moved email class

--- lib/server.rb
+++ lib/server.rb
@@ -18,6 +18,29 @@ class RichardStallmanVisitsForward < Sinatra::Application
     
   end
   
+  module Email
+    @queue = :send_email
+
+    def self.perform(email)
+       Pony.mail({
+          :to => 'email',
+          :from => 'confirmation@forward',
+          :subject => 'Thank you for registering',
+          :html_body => haml(:email),    
+          :via => :smtp,
+          :via_options => {
+            :address => 'smtp.sendgrid.net',
+            :port => '587',
+            :domain => 'heroku.com',
+            :user_name => ENV['SENDGRID_USERNAME'],
+            :password => ENV['SENDGRID_PASSWORD'],
+            :authentication => :plain,
+            :enable_starttls_auto => true
+          }
+        })
+    end
+  end
+  
 
   uri = URI.parse(ENV["REDISTOGO_URL"] || "http://localhost")
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
