44ce5da0ef335b90451c5b691431576afdd7f3f8
Jon Neale <jon.neale@uswitch.com>
2012-06-06 13:07:45 +0100

    Added pony and sendmail options to send confirmation email

--- lib/server.rb
+++ lib/server.rb
@@ -15,7 +15,28 @@ class RichardStallmanVisitsForward < Sinatra::Application
       @db ||= db 
       @db['attendees'] 
     end
+    
   end
+  
+  
+  get '/email' do
+    Pony.options = {
+      :to => 'jon.neale@forward.co.uk',
+      :subject => 'Thank you for registering',
+      :html_body => haml(:email),    
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'heroku.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
+    }
+  end
+
   
   get '/' do
     haml :index, :locals => {:confirmed => false}
